ca6c073eba845ab27634a04dc19a8d9a55750f5f
giriraj13 <giriraj@dotstark.com>
2022-07-25 12:46:45 +0530

    first test

---
+++ MEDIOClinic/ViewComponents/NavigationMenuViewComponent.cs
@@ -0,0 +1,49 @@
+﻿using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+
+using Microsoft.AspNetCore.Mvc;
+
+using CMS.Core;
+using CMS.DocumentEngine;
+using CMS.DocumentEngine.Routing;
+
+using Kentico.Content.Web.Mvc;
+
+using MEDIOClinic.Models;
+
+
+
+
+namespace CoreTutorial.ViewComponents
+{
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+        private readonly IPageRetriever pageRetriever;
+        private readonly IPageUrlRetriever pageUrlRetriever;
+
+        public NavigationMenuViewComponent(IPageRetriever pageRetriever, IPageUrlRetriever pageUrlRetriever)
+        {
+            this.pageRetriever = pageRetriever;
+            this.pageUrlRetriever = pageUrlRetriever;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            IEnumerable<TreeNode> menuItems = await pageRetriever.RetrieveAsync<TreeNode>(query => query
+            .MenuItems()
+            .NestingLevel(1)
+            .Columns("DocumentName", "NodeID", "NodeSiteID")
+            .WithPageUrlPaths()
+            .OrderByAscending("NodeOrder"));
+
+            IEnumerable<MenuItemViewModel> model = menuItems.Select(item => new MenuItemViewModel()
+            {
+                MenuItemText = item.DocumentName,
+                MenuItemRelativeUrl = pageUrlRetriever.Retrieve(item).RelativePath
+            });
+
+            return View(model);
+        }
+    }
+}
